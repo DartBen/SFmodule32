322e4486c5950dcd7733b3103db89761c3a743e6
DESKTOP-BBDMNJN\Dima <ryabchikk@rambler.ru>
2023-04-24 22:13:08 +0300

    логгер переписан на мидлвэр

---
+++ SFmodule32/SFmodule32/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace SFmodule32.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        ///  Middleware-компонент должен иметь конструктор, принимающий RequestDelegate
+        /// </summary>
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        ///  Необходимо реализовать метод Invoke  или InvokeAsync
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Для логирования данных о запросе используем свойства объекта HttpContext
+            string log = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}";
+            string pathToLoger = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "RequestLog.txt");
+            Console.WriteLine(log);
+
+            using (StreamWriter streamWriter = new StreamWriter(File.Open(pathToLoger, FileMode.Append)))
+            {
+                streamWriter.WriteLineAsync(log);
+            }
+
+            // Передача запроса далее по конвейеру
+            await _next.Invoke(context);
+        }
+    }
+}
--- SFmodule32/SFmodule32/Program.cs
+++ SFmodule32/SFmodule32/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+using SFmodule32.Middlewares;
 
 namespace SFmodule32
 {
@@ -18,20 +18,22 @@ namespace SFmodule32
 
             app.UseRouting();
 
-            app.Use(async (context, next) =>
-            {
-                // Для логирования данных о запросе используем свойства объекта HttpContext
-                string log = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}";
-                string pathToLoger = Path.Combine(app.Environment.ContentRootPath, "Logs", "RequestLog.txt");
-                Console.WriteLine(log + $"\n{pathToLoger}");
+            //app.Use(async (context, next) =>
+            //{
+            //    // Для логирования данных о запросе используем свойства объекта HttpContext
+            //    string log = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}";
+            //    string pathToLoger = Path.Combine(app.Environment.ContentRootPath, "Logs", "RequestLog.txt");
+            //    Console.WriteLine(log + $"\n{pathToLoger}");
 
-                using (StreamWriter streamWriter = new StreamWriter(File.Open(pathToLoger, FileMode.Append)))
-                {
-                    streamWriter.WriteLineAsync(log);
-                }
+            //    using (StreamWriter streamWriter = new StreamWriter(File.Open(pathToLoger, FileMode.Append)))
+            //    {
+            //        streamWriter.WriteLineAsync(log);
+            //    }
 
-                await next.Invoke();
-            });
+            //    await next.Invoke();
+            //});
+
+            app.UseMiddleware<LoggingMiddleware>();
 
             app.Map("/about", appBuilder => Endpoint.About(app, app.Environment));
             app.Map("/config", appBuilder => Endpoint.Config(app, app.Environment));
